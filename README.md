ce660ed3629d890852bc3700a23c1acec394cd09
wisemonkey <oranbusiness@gmail.com>
2020-04-04 20:58:02 -0700

    [add]  first page google: link, descript, name

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # google-first-page
  gets the url's and text of the first page of google givin search string
+
+
+
+## how to install 
+```
+pip install -r ./google-first-page/requirements.txt
+```
+
+## Build process
+
+### windows
+```
+pip freeze | findstr Google-Search-API >> .\google-first-page\requirements.txt
+```
+
+### *unix
+```
+pip freeze | grep Google-Search-API >> ./google-first-page/requirements.txt
+```
--- google-first-page/handler.py
+++ google-first-page/handler.py
@@ -2,12 +2,30 @@
 # github.com/wisehackermonkey
 # oranbusiness@gmail.com
 # 20200404
+# docs for Google-Search-API
+# https://github.com/abenassi/Google-Search-API
+from google import google 
+# NOTE: on github/pypi "google" is called Google-Search-API
+import json
 
-def handle(req):
+def handle(search_query):
     """handle a request to the function
     Args:
         req (str): request body
     """
+    result_json = []
 
-    return req
+    from google import google
+    num_page = 1
+    search_results = google.search(search_query, num_page)
+    for result in search_results:
+        # print(result.name)
+        # print(f"        {result.description}")
 
+        result_json.append( { "name": result.name, 
+                              "link": result.link, 
+                              "description": result.description})
+
+    return result_json
+
+# print(handle("hello world"))
